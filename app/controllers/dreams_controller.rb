1ef12eadcd8969ed3194b7a28de6f42072d5b5ed
ThatoneKurdishSE <razaljaf@gmail.com>
2021-03-13 14:56:12 -0700

    index, create, and destroy are in place. Missing U but will be sure to add soon.

--- app/controllers/dreams_controller.rb
+++ app/controllers/dreams_controller.rb
@@ -4,6 +4,21 @@ class DreamsController < ApplicationController
         @dreams = Dream.all
 
         render json: @dreams
+    end
 
+    def create
+        @dream = Dream.create(
+            title: params[:title],
+            content: params[:content],
+            date: params[:date]
+        )
+        render json: @dream, status: :created
+    end
+
+    def destroy
+        @dream = Dream.find(params[:id])
+        @dream.destroy
+
+        render "Like it was never even there!"
     end
 end
